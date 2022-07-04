e76e51b95e52648b00435d26ad4dbfd436571d3d
token <hejiale@xymind.cn>
2022-07-04 18:55:58 +0800

    初始化

---
+++ src/Tag/ITokenTag.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
+namespace Tag;
+
+public interface ITokenTag
+{
+    Task ConfigureServicesAsync(IServiceCollection context);
+
+    void ConfigureServices(IServiceCollection context);
+    
+    Task OnApplicationShutdownAsync(IApplicationBuilder context);
+
+    void OnApplicationShutdown(IApplicationBuilder context);
+}
---
+++ src/Tag/TokenTag.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
+namespace Tag;
+
+public abstract class TokenTag:ITokenTag
+{
+    public virtual Task ConfigureServicesAsync(IServiceCollection services)
+    {
+        ConfigureServices(services);
+        return Task.CompletedTask;
+    }
+
+    public virtual void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    public virtual Task OnApplicationShutdownAsync(IApplicationBuilder app)
+    {
+        OnApplicationShutdown(app);
+        return Task.CompletedTask;
+    }
+
+    public virtual void OnApplicationShutdown(IApplicationBuilder app)
+    {
+    }
+}
