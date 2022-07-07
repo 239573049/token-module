775e296b212d93b4e2ffe6b09e8257211b5d6878
token <239573049@qq.com>
2022-07-07 14:13:04 +0800

    扩展方法

---
+++ src/Token.Module/Extensions/TokenApplicationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Token.Module.Extensions;
+
+public static class TokenApplicationExtensions
+{
+    public static TService? GetService<TService>(this IApplicationBuilder app) =>
+        app.ApplicationServices.GetService<TService>();
+
+    public static IConfiguration? GetConfiguration(this IApplicationBuilder app) =>
+        app.ApplicationServices.GetService<IConfiguration>();
+
+    public static ILoggerFactory? GetLoggerFactory(this IApplicationBuilder app) =>
+        app.ApplicationServices.GetService<ILoggerFactory>();
+}
