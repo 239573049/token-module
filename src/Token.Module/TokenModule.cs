f72132ba1cc766d30849750986d470e1f30c9b68
token <239573049@qq.com>
2022-07-07 17:28:31 +0800

    增加Configure方法

--- src/Token.Module/TokenModule.cs
+++ src/Token.Module/TokenModule.cs
@@ -1,11 +1,22 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
+using System;
 
 namespace Token.Module;
 
-public abstract class TokenModule:ITokenModule
+public abstract class TokenModule : ITokenModule
 {
+    private IServiceCollection _serviceCollection;
+
+    private IServiceCollection ServiceCollection
+    {
+        get => this._serviceCollection != null
+                   ? this._serviceCollection
+                   : throw new Exception("在启动模块的时候ServiceCollection为空");
+        set => this._serviceCollection = value;
+    }
+
     public virtual Task ConfigureServicesAsync(IServiceCollection services)
     {
         ConfigureServices(services);
@@ -25,4 +36,10 @@ public abstract class TokenModule:ITokenModule
     public virtual void OnApplicationShutdown(IApplicationBuilder app)
     {
     }
+
+    protected void Configure<TOptions>(Action<TOptions> configureOptions) where TOptions : class =>
+        ServiceCollection.Configure(configureOptions);
+
+    protected void Configure<TOptions>(string name, Action<TOptions> configureOptions) where TOptions : class =>
+        ServiceCollection.Configure(name, configureOptions);
 }
